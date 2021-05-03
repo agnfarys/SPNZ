a16db95ad030c835c419819dd21375315b704893
agnfarys <80052221+agnfarys@users.noreply.github.com>
2021-05-03 17:12:45 +0200

    Wstępna wersja klas

---
+++ Akademik.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WpfApp1
+{
+	public class Akademik
+	{
+		int Pozar()
+        {
+			return 0;
+        }
+		int Gry()
+        {
+			return 0;
+        }
+		int Discord()
+        {
+			return 0;
+        }
+		int Etrapez()
+        {
+			return 0;
+        }
+		int Zajecia()
+        {
+			return 0;
+        }
+		int Grupka()
+        {
+			return 0;
+        }
+		int Spij()
+        {
+			return 0;
+        }
+	}
+}
---
+++ Event.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace WpfApp1
+{
+	public class Event
+	{
+		bool dostepnosc;
+        int liczba_odwiedzin;
+        int zaliczenie;
+
+    //Wyspa
+    void Piwo()
+{
+
+    }
+
+    void Flany()
+{
+
+}
+
+    void Integracja()
+{
+
+}
+
+    void Interwencja()
+{
+
+}
+
+    //Zaliczenie
+    void Kolokwium()
+    {
+
+    }
+
+    void Egzamin()
+    {
+
+    }
+}
+}
+
