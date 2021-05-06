120009516add04e113a53c899aa0168fac6835e1
Mikołaj Zielonka <58637689+MZielonkaa@users.noreply.github.com>
2021-05-06 20:06:02 +0200

    Add files via upload

---
+++ Akademik.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SPNZ
+{
+	public class Akademik
+	{
+		int Pozar()
+		{
+			return 0;
+		}
+		int Gry()
+		{
+			Student.ZmianaEnergia(-20);
+			Student.ZmianaCheci(30);
+			Student.ZmianaGodzina(2);
+			return 0;
+		}
+		void Discord()
+		{
+			Student.ZmianaEnergia(-10);
+			Student.ZmianaCheci(10);
+			Student.ZmianaWspolpraca(5);
+			Student.ZmianaGodzina(2);
+		}
+		void Etrapez()
+		{
+			Student.ZmianaEnergia(-20);
+			Student.ZmianaCheci(-15);
+			Student.ZmianaGodzina(2);
+			Student.ZmianaWiedza(25);
+
+
+		}
+		void Zajecia()
+		{
+			Student.ZmianaEnergia(-30);
+			Student.ZmianaCheci(-20);
+			Student.ZmianaGodzina(3);
+			Student.ZmianaWiedza(40);
+		}
+		void Grupka()
+		{
+			
+		}
+
+		void Spij()
+		{
+
+		}
+	}
+}
