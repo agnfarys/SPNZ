e3987a5c77420a5adad58a23d5313f8fc6e4d01d
agnfarys <80052221+agnfarys@users.noreply.github.com>
2021-06-10 14:28:10 +0200

    Add files via upload

---
+++ Update 10.06/Akademik.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Media;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SPNZ
+{
+	public class Akademik
+	{
+		public static bool Pozar()
+		{
+			Random poz = new Random();
+			int los = poz.Next(0, 1000);
+
+			int p = 150, k=500, s=850; 
+
+			if ((los == p) || (los==k) || (los==s))
+			{
+				Student.ZmianaGodzina(6);
+				Student.ZmianaCheci(-20);
+				Student.ZmianaEnergia(-20);
+				MediaPlayer mplayer = new MediaPlayer();
+				mplayer.Open(new Uri("XDDDD.wav", UriKind.Relative));
+				mplayer.Play();
+				MessageBox.Show("Wybuchł pożar!!!!", "Pali się", MessageBoxButton.OK, MessageBoxImage.Warning);
+				return true;
+			}
+			return false;
+		}
+		public static void Gry()
+		{
+			MediaPlayer mplayer = new MediaPlayer();
+			mplayer.Open(new Uri("amogus.wav", UriKind.Relative));
+			mplayer.Play();
+			Student.ZmianaEnergia(-15);
+			Student.ZmianaCheci(3);
+			Student.ZmianaGodzina(3);
+			Student.ZmianaTrzezwosc(5);
+		}
+		public static void Discord()
+		{
+			MediaPlayer mplayer = new MediaPlayer();
+			mplayer.Open(new Uri("discord.wav", UriKind.Relative));
+			mplayer.Play();
+			Student.ZmianaEnergia(-15);
+			Student.ZmianaCheci(5);
+			Student.ZmianaWspolpraca(2);
+			Student.ZmianaGodzina(3);
+			Student.ZmianaTrzezwosc(5);
+		}
+		public static void Etrapez()
+		{
+			MediaPlayer mplayer = new MediaPlayer();
+			mplayer.Open(new Uri("etrapez.wav", UriKind.Relative));
+			mplayer.Play();
+			Student.ZmianaEnergia(-20);
+			Student.ZmianaCheci(-10);
+			Student.ZmianaGodzina(4);
+			Student.ZmianaWiedza(5);
+			Student.ZmianaTrzezwosc(5);
+		}
+		public static void Zajecia()
+		{
+			MediaPlayer mplayer = new MediaPlayer();
+			mplayer.Open(new Uri("teams.wav", UriKind.Relative));
+			mplayer.Play();
+			Student.ZmianaEnergia(-30);
+			Student.ZmianaCheci(-20);
+			Student.ZmianaGodzina(6);
+			Student.ZmianaWiedza(10);
+		}
+		public static void Spij()
+		{
+			MediaPlayer mplayer = new MediaPlayer();
+			mplayer.Open(new Uri("chrapanie.wav", UriKind.Relative));
+			mplayer.Play();
+			Student.ZmianaEnergia(30);
+			Student.ZmianaGodzina(6);
+			Student.ZmianaCheci(12);
+			Student.ZmianaTrzezwosc(15);
+		}
+	}
+}
---
+++ Update 10.06/Instrukcja.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SPNZ
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Opcje.xaml
+    /// </summary>
+    public partial class Instrukcja : Window
+    {
+        public Instrukcja()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            this.Close();
+
+        }
+    }
+}
